691465f219f2b5418e0c7f3711c658b5758cb3aa
nehapanth14 <32521764+nehapanth14@users.noreply.github.com>
2018-07-06 12:15:24 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+a=raw_input("enter the character:")
+f="a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+if a in ('a','e','i','o','u'):
+    print("vowels")
+elif a in f:
+        print("consonant")
+else:
+    print("invalid input")
