9cffe89d2ae66684cb03814204596cd96e80a521
nehapanth14 <32521764+nehapanth14@users.noreply.github.com>
2018-07-22 22:57:18 +0530

    Add files via upload

--- 2p.py
+++ 2p.py
@@ -1,6 +1,11 @@
 a=int(raw_input(""))
 fact=1
 while a>0:
-    fact=fact*a
-    a=a-1
+    if a==0:
+        fact=1
+        print fact
+        break
+    else:
+        fact=fact*a
+        a=a-1
 print fact
